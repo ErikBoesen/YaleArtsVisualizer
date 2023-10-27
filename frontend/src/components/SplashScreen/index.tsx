fdf7ef7eb419c6e4ec06ad066aea844b7e9ee1d5
evan kirkiles <kirkilese@gmail.com>
2023-10-27 12:27:58 -0400

    Added basic always-shown splash screen.

--- frontend/src/components/SplashScreen/index.tsx
+++ frontend/src/components/SplashScreen/index.tsx
@@ -9,33 +9,49 @@
 import YAMLogo from "@/assets/svg/YAMLogo";
 import s from "./SplashScreen.module.scss";
 import { useEffect, useReducer, useState } from "react";
-import { Transition } from "transition-hook";
+import { Transition, useTransition } from "transition-hook";
 
 const SPLASH_SCREEN_STORAGE_KEY = "yam-splash-done";
 
 export default function SplashScreen() {
-  const [showSplash, dispatch] = useReducer(
+  const [showSplash, setShowSplash] = useReducer(
     (prev: boolean, newState: boolean) => {
-      localStorage.setItem(SPLASH_SCREEN_STORAGE_KEY, JSON.stringify(newState));
+      // localStorage.setItem(
+      //   SPLASH_SCREEN_STORAGE_KEY,
+      //   JSON.stringify(!newState)
+      // );
       return newState;
     },
     typeof window !== "undefined",
-    () => !localStorage.getItem(SPLASH_SCREEN_STORAGE_KEY)
+    () =>
+      typeof window !== "undefined"
+        ? localStorage.getItem(SPLASH_SCREEN_STORAGE_KEY) !== "true"
+        : false
   );
+  const { shouldMount, stage } = useTransition(showSplash, 700);
+  useEffect(() => {
+    const timeout = setTimeout(() => setShowSplash(false), 1000);
+    return () => clearTimeout(timeout);
+  }, [showSplash]);
 
   return (
-    <Transition state={showSplash} timeout={300}>
-      {(stage, shouldMount) =>
-        shouldMount && (
-          <section className={s.container} suppressHydrationWarning>
-            <YAMLogo className={s.logo} />
-            <div className={s.sublogo}>
-              <p>Yale Arts Map</p>
-              <p>SWE F23</p>
-            </div>
-          </section>
-        )
-      }
-    </Transition>
+    <section
+      className={s.container}
+      style={{
+        opacity:
+          typeof window !== "undefined" && (stage === "leave" || !shouldMount)
+            ? 0
+            : undefined,
+        pointerEvents:
+          typeof window !== "undefined" && !shouldMount ? "none" : undefined,
+      }}
+      suppressHydrationWarning
+    >
+      <YAMLogo className={s.logo} />
+      <div className={s.sublogo}>
+        <p>Yale Arts Map</p>
+        <p>SWE F23</p>
+      </div>
+    </section>
   );
 }
