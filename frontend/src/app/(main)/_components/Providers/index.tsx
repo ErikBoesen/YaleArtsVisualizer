c88f9ff5732cf6e49019e5c95bab8518d06b3a23
Evan Kirkiles <kirkilese@gmail.com>
2023-10-28 16:22:27 -0400

    Added Jotai.

--- frontend/src/app/(main)/_components/Providers/index.tsx
+++ frontend/src/app/(main)/_components/Providers/index.tsx
@@ -8,6 +8,7 @@
 "use client";
 
 import { PropsWithChildren } from "react";
+import { Provider } from "jotai";
 
 /**
  * This client component provides all of the providers we need in the main layout.
@@ -15,5 +16,5 @@ import { PropsWithChildren } from "react";
  * For example, we'll add here the query client for fetching data.
  */
 export default function Providers({ children }: PropsWithChildren) {
-  return <>{children}</>;
+  return <Provider>{children}</Provider>;
 }
---
+++ frontend/src/app/state.ts
@@ -0,0 +1,8 @@
+/*
+ * state.ts
+ * Author: evan kirkiles
+ * Created on: Sat Oct 28 2023
+ * 2023 Yale SWE
+ *
+ * This file defines the global state atoms needed for our application.
+ */
